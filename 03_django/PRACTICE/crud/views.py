6f769e7736ec6c122fb776ac5936228b2b17f085
verba-neo <verba.it.corp@gmail.com>
2023-10-10 16:13:08 +0900

    practice w/ form

--- 03_django/PRACTICE/crud/views.py
+++ 03_django/PRACTICE/crud/views.py
@@ -1,25 +1,27 @@
 from django.shortcuts import render, redirect
 
 from .models import Student
+from .forms import StudentForm
 
 def new(request):
     # 새로운 student 를 생성하기 위한 <form>(HTML) => 작성데이터는 create 함수로 보내야 함
-    return render(request, 'crud/new.html')
+    form = StudentForm()
+    return render(request, 'crud/new.html', {
+        'form': form,
+    })
 
 
 def create(request):
     # new.html 에서 넘어온 데이터를 실제 저장
-    student = Student()
-    student.name = request.GET['student_name']
-    student.age = request.GET['student_age']
-    student.major = request.GET['student_major']
-    student.description = request.GET['student_description']
-    student.save()  # 저장 됨! => id(pk)가 생김
-
-    return redirect('crud:detail', student.pk)
-    # return redirect(f'/school/{student.pk}/')
-
-
+    form = StudentForm(data=request.POST)
+    if form.is_valid():
+        student = form.save()
+        return redirect('crud:detail', student.pk)
+    else:
+        return render(request, 'crud/new.html', {
+            'form': form,
+        })
+    
 
 def index(request):
     # 전체 학생 목록 확인
@@ -40,20 +42,27 @@ def detail(request, pk):
 def edit(request, pk):
     # student 를 수정하기 위한 <form>(HTML) => 작성데이터는 update 함수로 보내야 함
     student = Student.objects.get(pk=pk)
+    form = StudentForm(instance=student)
+
     return render(request, 'crud/edit.html', {
         'student': student,
+        'form': form,
     })
 
 
 def update(request, pk):
     # edit.html 에서 넘어온 데이터를 실제 저장
     student = Student.objects.get(pk=pk)
-    student.name = request.GET['student_name']
-    student.age = request.GET['student_age']
-    student.major = request.GET['student_major']
-    student.description = request.GET['student_description']
-    student.save() 
-    return redirect('crud:detail', student.pk)
+    form = StudentForm(data=request.POST, instance=student)
+    if form.is_valid():
+        student = form.save()
+        return redirect('crud:detail', student.pk)
+    else:
+        return render(request, 'crud/new.html', {
+            'form': form,
+            'student': student,
+        })
+    
 
 
 
